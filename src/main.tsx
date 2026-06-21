6ff789812d608bc027f9fbcbf00dfdfd2fb71030
emaa95 <chiletti.ema@gmail.com>
2026-06-21 20:26:23 -0300

    chore(main): eliminación de React StrictMode en entry point

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  
     <RouterProvider router={router}/>
-  </StrictMode>,
+ 
 )
